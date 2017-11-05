b3da2385956b8a2e1fe20888b54a05e390108ffa
linux740305 <bata2016121@yahoo.com>
2017-11-05 20:36:51 +0800

    Update 備份文件.py

--- 備份文件.py
+++ 備份文件.py
@@ -7,7 +7,11 @@ cp_File_Name = input("請輸入要備份的文件名:")
 cp_read = (cp_File_Name, "r")
 #2.打開複製的文件
 
-cp_write = open(newname.txt,"w")
+position = cp_File_Name.rfind(".")
+new_file_name = f [0:position] + "[new-cp]" + cp_File_Name [position:]
+#附件加上
+
+cp_write = open(new_file_name.txt,"w")
 #3.創建一個新的文件
 
 content = cp_read.read()
